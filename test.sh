c9ac3af26143066e532bf0282c052ec241a40d6e
Dan Balasescu <smoogipoo@smgi.me>
2022-01-13 13:15:52 +0900

    Fix test script

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 dotnet tool restore
-dotnet jb inspectcode $(pwd)/osu.Framework.Tests/osu.Framework.Tests.csproj --no-build --debug --loglevel=TRACE --logfile=$(pwd)/inspectcode.log --output=$(pwd)/inspectcodereport.xml --cachesDir=$(pwd)/inspectcode &
+dotnet jb inspectcode $(pwd)/osu.Framework.Tests.csproj --no-build --debug --loglevel=TRACE --logfile=$(pwd)/inspectcode.log --output=$(pwd)/inspectcodereport.xml --cachesDir=$(pwd)/inspectcode
